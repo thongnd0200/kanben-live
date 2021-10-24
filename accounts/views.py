3094d63ff28a9a991550da03eaf6a093b5003590
ndthong <65402387+thongnd0200@users.noreply.github.com>
2021-10-25 00:05:44 +0700

    add all files

--- accounts/views.py
+++ accounts/views.py
@@ -81,7 +81,9 @@ class RegisterAPI(APIView):
             token = RefreshToken.for_user(user).access_token
             current_site = get_current_site(request).domain
             relativeLink = reverse('email-verify')
-            absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+            # absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+            absurl = 'http://localhost:3000/email-verify/'+str(token)
+
             email_body = 'Hi '+user.username + \
                 ' Use the link below to verify your email \n' + absurl
             send_data = {'email_body': email_body, 'to_email': user.email,
